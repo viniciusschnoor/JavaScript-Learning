3028d966e4e445ed7f0bc73887177a2144a54ca1
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-09 23:47:38 -0300

    Update js007.js

--- 10/js/js007.js
+++ 10/js/js007.js
@@ -3,11 +3,22 @@ var tn2 = document.querySelector('input#txtn2')
 var btn = document.getElementById('btnSomar')
 var res = window.document.getElementById('res')
 
+btn.addEventListener('mouseenter', mouseSobre)
+btn.addEventListener('mouseout', mouseFora)
 btn.addEventListener('click', somar)
 
 function somar() {
+    btn.style.backgroundColor = 'rgb(128,255,128)'
     var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
     var s = n1 + n2
     res.innerHTML = s
+}
+
+function mouseSobre() {
+    btn.style.backgroundColor = 'rgb(192,255,192)'
+}
+
+function mouseFora() {
+    btn.style.backgroundColor = 'white'
 }
