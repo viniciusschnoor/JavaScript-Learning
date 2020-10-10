c35a07dfb6bd380a53602489d882c981b1c910be
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-10 11:47:20 -0300

    Ex 08 e 09

---
+++ 11/js008.js
@@ -0,0 +1,6 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida e foi MULTADO!`)
+}
+console.log(`Dirija sempre utilizando cinto de segurança`)
