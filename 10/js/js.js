f85ed026f4eb8edaf83912ecf46e58e21b21bd44
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-09 22:35:53 -0300

    novas funcoes

--- 10/js/js.js
+++ 10/js/js.js
@@ -1,2 +1,21 @@
-var d = window.document.getElementById('area')
-d.style.color = "white"
+var a = window.document.getElementById('area')
+function clicar() {
+    /*
+        1º OnClick na HTML
+        2º Criado a Função no JS
+    */
+   a.innerHTML = 'Clicou!'
+   a.style.backgroundColor = 'red'
+}
+function entrar() {
+    a.innerHTML = 'Entrou!'
+    a.style.backgroundColor = 'green'
+}
+
+function sair() {
+    a.innerHTML = 'Saiu!'
+    a.style.backgroundColor = 'black'
+
+}
+
+
