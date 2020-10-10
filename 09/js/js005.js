d4ea7428272a3d7f0d9f82eb9b40a21123afe507
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-09 23:54:26 -0300

    Pastas Organizadas - 09

---
+++ 09/js/js005.js
@@ -0,0 +1,10 @@
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[1]
+/*
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'
+window.document.getElementById('msg').innerText = 'Olá'
+*/
+var d = window.document.querySelector('div#msg')
+d.style.color = 'blue'
