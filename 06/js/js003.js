3875bbed8164338e755ce34ed0bcfa2e3c06361d
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-11 22:36:41 -0300

    Recolocação de Arquivos

---
+++ 06/js/js003.js
@@ -0,0 +1,9 @@
+var n1 = Number(window.prompt('Digite um número')) // Recebendo string e formatando para number
+var n2 = window.prompt('Digite outro número') // Recebendo string
+var r = n1 + Number(n2)
+window.alert(`A soma entre os números ${n1} e ${n2} é ${r}`)
+/*
+    Símbolo de adição serve para concatenação e adição
+    number + number >> Adição
+    string + string >> Concatenação
+*/
---
+++ 06/js/js004.js
@@ -0,0 +1,3 @@
+var nome = window.prompt('Qual seu nome?')
+document.write(`Seu nome, <strong>${nome}</strong>, tem ${nome.length} letras<br/>`)
+document.write(`Seu nome em maíusculas fica ${nome.toUpperCase()}`)
