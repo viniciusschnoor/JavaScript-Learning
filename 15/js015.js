6f40cacf5b0a1ffe61f94e04bfe60f9f09f88735
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-15 18:49:25 -0300

    Update js015.js

--- 15/js015.js
+++ 15/js015.js
@@ -1,9 +1,24 @@
-var num = [5, 8, 9, 3]
+let num = [5, 8, 2, 9, 3]
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(num)
 
-num[4]=6
+console.log(`O vetor tem ${num.length} posições`)
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(`O primeiro valor do vetor é ${num[1]}`)
 
+num.sort()
+
+console.log(num)
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`O primeiro valor do vetor é ${num[1]}`)
+
+num.push(1)
+
+console.log(num)
+
+num.sort()
+
+console.log(num)
 
