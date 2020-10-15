c806b7f6edd31473d1ef2d2b273373317e72a73d
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-15 16:41:26 -0300

    Update 014ex01.js

--- Exercicios Fixação/js/014ex01.js
+++ Exercicios Fixação/js/014ex01.js
@@ -5,15 +5,28 @@ function contar() {
     let fresultado = document.getElementById('resultado')
 
     if (finicio.value.length == 0 || ffim.value.length == 0 || fintervalo.value.length == 0) {
-        alert('ERRO')
+        fresultado.innerHTML = 'Impossível Contar'
+        //alert('ERRO')
     } else {
-        fresultado.innerHTML = "Contando... "
+        fresultado.innerHTML = "Contando...<br>"
         let i = Number(finicio.value)
         let f = Number(ffim.value)
         let p = Number(fintervalo.value)
-
-        for ( let c = i ; c <= f ; c += p ) {
-            fresultado.innerHTML += `\u{1F44B} ${c} `
+        if (p <= 0) {
+            fresultado.innerHTML = 'O intervalo deve ser um número inteiro maior que zero, considerarei intervalo igual a um<br><br>Contando...<br>'
+            p = 1
         }
+        if (i < f) {
+            // Contagem Progressiva
+            for ( let c = i ; c <= f ; c += p ) {
+                fresultado.innerHTML += `\u{1F449} ${c} `
+            }
+        } else {
+            // Contagem Regressiva
+            for ( let c = i ; c >= f ; c -= p ) {
+                fresultado.innerHTML += `\u{1F449} ${c} `
+            }
+        }
+        fresultado.innerHTML += ` \u{1F3C1}`
     }
 }
