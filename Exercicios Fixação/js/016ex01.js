0c2dfff6a29cf29bc43e4a221f209eabf4ddbb92
Vinícius Schnoor <contato@viniciussantos.adm.br>
2020-10-15 22:25:11 -0300

    Exercio Final

--- Exercicios Fixação/js/016ex01.js
+++ Exercicios Fixação/js/016ex01.js
@@ -1,3 +1,4 @@
+//FUNÇÃO PRINCIPAL
 let principal = document.querySelector('section#principal')
 let formulario = document.querySelector('div#formulario')
 let fnum = document.querySelector('input#fnum')
@@ -7,6 +8,23 @@ let resultado = document.createElement('div')
 let presultado = document.createElement('p')
 resultado.setAttribute('id','resultado')
 presultado.setAttribute('id','presultado')
+
+function adicionar() {
+    if ( verNumer(fnum.value) && !verLista(fnum.value, valores) ) {
+        valores.push(Number(fnum.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${fnum.value} adicionado.`
+        flista.appendChild(item)
+        let itenssalvos = valores.length
+        flista.setAttribute('size',itenssalvos)
+        presultado.innerHTML = ''
+        principal.removeChild(resultado)
+    } else {
+        alert('Valor inválido ou já encontrado na lista')
+    }
+    fnum.value = ''
+    fnum.focus()
+}
 
 function verNumer(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
@@ -24,12 +42,14 @@ function verLista(n, l) {
     }
 }
 
-function adicionar() {
-    //principal.appendChild(resultado)
-    //resultado.appendChild(presultado)
-    if ( verNumer(fnum.value) && !verLista(fnum.value, valores) ) {
-        
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
     } else {
-        alert('Valor inválido ou já encontrado na lista')
+        principal.appendChild(resultado)
+        resultado.appendChild(presultado)
+        presultado.innerHTML = `<strong>Resultado:</strong><br>`
+        let totalelementos = valores.length
+        presultado.innerHTML += `<ul><li>Foram incluídos ${totalelementos} números</li></ul>`
     }
 }
